653c4148ee6f531360a27ce59f539529b982c121
Max <maxim1006@list.ru>
2015-11-03 18:51:28 +0300

    Updated yield promise

--- 10-generators/05-yield-promise-callback.js
+++ 10-generators/05-yield-promise-callback.js
@@ -10,40 +10,65 @@ var fs = require('mz/fs'); //нужен, чтобы fs возвращал про
 //  file -> return contents
 //  
 //  
-/*
- function readAsync(path, callback) {
 
- fs.stat(path, function(err, stat) {
- if (err) return callback(err);
 
- if (stat.isDirectory()) {
- fs.readdir(path, function(err, files) {
- if (err) return callback(err);
- callback(null, files);
- });
- } else {
- fs.readFile(path, function(err, content) {
- if (err) return callback(err);
- callback(null, content);
- });
- }
 
- });
- }
- */
-
-function readAsync(path) {
+/*Через промисы*/
+/*function readAsync(path) {
     return fs.stat(path)
         .then(stat => {
-            return stat.isDirectory() ? fs.readdir(path) : fs.readFile(path);
+            if (stat.isDirectory()) {
+                return fs.readdir(path);
+            } else {
+                return fs.readFile(path);
+            }
+        })
+        .then((files) => {
+
+            if (Array.isArray(files)) {
+                let arr = [];
+
+                for (let i = 0; i < files.length; i++ ) {
+                    arr.push(fs.readFile(files[i]));
+                }
+
+                return Promise.all(arr)
+            } else {
+                return files;
+            }
+
         });
 }
 
-readAsync("sfdg")
+
+readAsync(__dirname)  //"./test.txt"
     .then(function (res) {
-        console.log(arguments);
+        console.log(res.length);
+
+        let size = 0;
+
+        res.forEach((item) => {
+            size += item.length;
+        });
+
+        console.log(size);
     })
-    .catch(console.log);
+    .catch(console.log);*/
+
+
+
+/*Через co*/
+/*
+function readAsync(path) {
+    co(() => {
+        let
+            });
+}
+*/
+
+
+
+
 
 
 //у объекта process, когда какой-либо промис сгенерировал ошибку и нет catch, то ее можно отловить тут
